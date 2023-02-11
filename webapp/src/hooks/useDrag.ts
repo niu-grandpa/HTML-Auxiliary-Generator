ac8ff4f7f4e1a59293f61936a1001cc56286c17a
niu-grandpa <2864103063@qq.com>
2023-02-11 20:02:16 +0800

    实现拖拽功能

--- webapp/src/hooks/useDrag.ts
+++ webapp/src/hooks/useDrag.ts
@@ -1,39 +1,104 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { RefObject, useCallback, useEffect, useRef } from 'react';
 
 /**
  * useDrag
  * @description 为元素绑定拖拽事件和计算其移动位置
- * @param {HTMLElement} target 被拖拽的目标元素
- * @param {HTMLElement} parent 目标元素的父节点，提供可移动范围空间
+ * @param {HTMLElement | null} target 被拖拽的目标元素
+ * @param {HTMLElement | null} parent 目标元素的父节点，提供可移动范围空间
  * @param {Function} callback
  */
 export function useDrag(
-  target: HTMLElement,
-  parent: HTMLElement,
+  target: RefObject<HTMLElement | null>,
+  parent: RefObject<HTMLElement | null>,
   callback: (x: number, y: number) => void
 ) {
-  const targetRef = useRef(target);
-  const parentRef = useRef(parent);
+  const targetRef = useRef(target.current);
+  const parentRef = useRef(parent.current);
   const callbackRef = useRef(callback);
+  const dashedElm = useRef(document.createElement('div'));
 
-  const dragging = useCallback((event: MouseEvent) => {
-    const x = event.clientX - targetRef.current.offsetWidth / 2;
-    const y = event.clientY - targetRef.current.offsetHeight / 2;
-    // 计算是否最大边界溢出
-    callbackRef.current?.(x, y);
+  const createDashed = useCallback((elm: HTMLElement, x: number, y: number) => {
+    if (dashedElm.current === null) {
+      dashedElm.current = document.createElement('div');
+    }
+    const dashed = dashedElm.current;
+    dashed.id = 'Move_Elm_Placeholder';
+    dashed.style.width = `${elm.offsetWidth}px`;
+    dashed.style.height = `${elm.offsetHeight}px`;
+    dashed.style.transform = `translate(${x}px, ${y}px)`;
+    dashed.style.boxShadow = '0 2px 0 rgb(0 0 0 / 2%)';
+    dashed.style.border = '1px dashed #1677ff';
+    elm.after(dashed);
   }, []);
 
-  const draggStar = useCallback(() => {
-    parentRef.current.addEventListener('mousemove', dragging);
-  }, [dragging]);
+  const destoryDashed = useCallback(() => {
+    if (dashedElm.current === null) return;
+    parentRef.current!.removeChild(dashedElm.current);
+    dashedElm.current = null as any;
+  }, []);
 
-  const dragEnd = useCallback(() => {
-    parentRef.current.removeEventListener('mousemove', dragging);
-    targetRef.current.removeEventListener('mouseup', dragEnd);
-  }, [dragging]);
+  const whenMouseDown = useCallback(
+    (ev: MouseEvent) => {
+      const { offsetLeft, offsetTop } = targetRef.current!;
+      // 鼠标点击物体那一刻相对于物体左侧边框的
+      // 距离=点击时的位置相对于浏览器最左边的距离-物体左边框相对于浏览器最左边的距离
+      const diffX = ev.clientX - offsetLeft;
+      const diffY = ev.clientY - offsetTop;
+      createDashed(targetRef.current!, offsetLeft, offsetTop);
+      return [diffX, diffY];
+    },
+    [createDashed]
+  );
 
   useEffect(() => {
-    targetRef.current.addEventListener('mousedown', draggStar);
-    targetRef.current.addEventListener('mousedown', dragEnd);
-  }, [target, parent, dragEnd, draggStar, dragging]);
+    targetRef.current = target.current;
+    parentRef.current = parent.current;
+
+    if (targetRef.current === null && parentRef.current === null) return;
+
+    const targetElm = targetRef.current!;
+    const parentElm = parentRef.current!;
+
+    const onMouseDown = (ev: MouseEvent) => {
+      ev.stopPropagation();
+
+      const [diffX, diffY] = whenMouseDown(ev);
+      const onMouseMove = (ev: MouseEvent) => {
+        ev.stopPropagation();
+        let moveX = ev.clientX - diffX;
+        let moveY = ev.clientY - diffY;
+        if (moveX < 0) {
+          moveX = 0;
+        } else if (moveX > parentElm.offsetWidth - targetElm.offsetWidth) {
+          moveX = parentElm.offsetWidth - targetElm.offsetWidth;
+        }
+        if (moveY < 0) {
+          moveY = 0;
+        } else if (moveY > parentElm.offsetHeight - targetElm.offsetHeight) {
+          moveY = parentElm.offsetHeight - targetElm.offsetHeight;
+        }
+        callbackRef.current(moveX, moveY);
+        targetElm.classList.add('view-opts-box-item-move');
+      };
+
+      const onCleanup = (ev: MouseEvent) => {
+        ev.stopPropagation();
+        destoryDashed();
+        targetElm.classList.remove('view-opts-box-item-move');
+        parentElm.removeEventListener('mousemove', onMouseMove);
+        parentElm.removeEventListener('mouseleave', onCleanup);
+        parentElm.removeEventListener('mouseup', onCleanup);
+      };
+
+      parentElm.addEventListener('mousemove', onMouseMove);
+      parentElm.addEventListener('mouseleave', onCleanup);
+      parentElm.addEventListener('mouseup', onCleanup);
+    };
+
+    targetElm.addEventListener('mousedown', onMouseDown);
+
+    return () => {
+      targetElm.removeEventListener('mousedown', onMouseDown);
+    };
+  }, [target, parent, destoryDashed, whenMouseDown]);
 }
