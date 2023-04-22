65cb9677118c1beca0b2fa8bf44b5dc5fc90e628
niu-grandpa <2864103063@qq.com>
2023-04-22 13:18:37 +0800

    优化生成node-key算法

--- webapp/src/core/utils.ts
+++ webapp/src/core/utils.ts
@@ -1,6 +1,6 @@
 import { TreeDataNode } from 'antd';
 import { CSSProperties } from 'react';
-import { NodeType, savedKeys as SAVED_KEYS } from './runtime-generate';
+import { NodeType } from './runtime-generate';
 
 export type VNode = {
   type: NodeType;
@@ -17,14 +17,8 @@ export type VNode = {
   } | null;
 };
 
-let uid = 0,
-  timestamp = Date.now();
 export function createNodeKey(): string {
-  while (SAVED_KEYS.includes(uid)) {
-    ++uid;
-  }
-  SAVED_KEYS.push(uid);
-  return `${uid}-${timestamp++}`;
+  return getHash();
 }
 
 // 解决复制的节点 key 冲突
@@ -60,4 +54,112 @@ export function createDragVnode(
     children,
     actualPos,
   };
+}
+
+export function getHash() {
+  //定义一个时间戳，计算与1970年相差的毫秒数  用来获得唯一时间
+  const timestamp = new Date().valueOf();
+  const myRandom = randomWord(false, 4, 5);
+  const hashcode = getHashCode(myRandom + timestamp.toString());
+  return `${myRandom}_${hashcode}`;
+}
+
+//产生一个hash值，只有数字，规则和java的hashcode规则相同
+function getHashCode(str: string) {
+  const BASE_HASH = 1013;
+  const HASH_CODE = 5381;
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = BASE_HASH * hash + str.charCodeAt(i);
+    if (hash > HASH_CODE - 1) hash %= HASH_CODE; //java int溢出则取模
+  }
+  return hash;
+}
+
+//时间戳来自客户端，精确到毫秒，但仍旧有可能在在多线程下有并发，
+//尤其hash化后，毫秒数前面的几位都不变化，导致不同日期hash化的值有可能存在相同，
+//因此使用下面的随机数函数，在时间戳上加随机数，保证hash化的结果差异会比较大
+/*
+ ** randomWord 产生任意长度随机字母数字组合
+ ** randomFlag-是否任意长度 min-任意长度最小位[固定位数] max-任意长度最大位
+ ** 用法  randomWord(false,6);规定位数 flash
+ ** randomWord(true,3，6);长度不定，true
+ ** arr变量可以把其他字符加入，如以后需要小写字母，直接加入即可
+ */
+function randomWord(randomFlag: boolean, min: number, max: number) {
+  let str = '';
+  let range = min;
+  const arr = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  // 随机产生
+  if (randomFlag) {
+    range = Math.round(Math.random() * (max - min)) + min;
+  }
+  for (let i = 0; i < range; i++) {
+    const pos = Math.round(Math.random() * (arr.length - 1));
+    str += arr[pos];
+  }
+  return str;
 }
