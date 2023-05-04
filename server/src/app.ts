3dfbd81d97fc0c1a4e7f68063c19369c278c700a
niu-grandpa <2864103063@qq.com>
2023-05-04 12:30:15 +0800

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,11 +6,9 @@ const port = 8080;
 const app = express();
 
 app
+  .use(express.static('public'))
   .use('/', page)
   .use('/api', api)
-  .use('*', (req, res) => {
-    res.status(404).render('404', { url: req.originalUrl });
-  })
   .listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
--- server/src/routes/page.ts
+++ server/src/routes/page.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
-import path from 'path';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.sendFile(path.join(__dirname, '../views/1.html'));
+router.get('/', (req, res) => res.redirect('/'));
+router.get('/home', (req, res) => {
+  // todo 发送对应静态文件
+});
+router.get('/tutorial', (req, res) => {
+  // todo 发送对应静态文件
+});
+router.get('/user-center', (req, res) => {
+  // todo 发送对应静态文件
+});
+router.get('*', (req, res) => {
+  // todo 发送对应静态文件
 });
 
 export { router };
